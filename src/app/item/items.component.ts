99119d2fab5f5e40080fd29ad9389a3bdc3a43b9
Dorian Aldair Oliveros 0909-17-8081 <aldair_oliveros09@hotmail.es>
2021-09-11 13:16:50 -0600

    creando plantilla Login

--- src/app/item/items.component.ts
+++ src/app/item/items.component.ts
@@ -3,9 +3,11 @@ import { Component, OnInit } from '@angular/core'
 import { Item } from './item'
 import { ItemService } from './item.service'
 
+
 @Component({
   selector: 'ns-items',
   templateUrl: './items.component.html',
+  styleUrls: ["./items.component.css"]
 })
 export class ItemsComponent implements OnInit {
   items: Array<Item>
